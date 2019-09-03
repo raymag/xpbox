c1e7a208514864878784e4a93f4e1741aa664716
raymag <prayk11@outlook.com>
2019-09-03 14:19:53 -0300

    .

--- Scripts/Python/Anagrama/anagram.py
+++ Scripts/Python/Anagrama/anagram.py
@@ -12,14 +12,44 @@ def findAnagrams(word):
                 tmp.append(perm[:i] + word[0:1] + perm[i:])
         return tmp
 
+def findAnagramsPrefix(word, prefix):
+	if len(word) <=1:
+		if word == prefix:
+			return word
+		else:
+			return list()
+	else:
+		tmp = list()
+		for perm in findAnagrams(word[1:]):
+			for i in range(len(word)):
+				anagram = perm[:i] + word[0:1] + perm[i:]
+				if anagram[:len(prefix)] == prefix:
+					tmp.append(anagram)
+		return tmp
+
 @click.group()
 def main():
 	pass
 
 @main.command()
 @click.argument('word')
+@click.argument('prefix')
 @click.option('--inline', default=False, help="If True, it will return the anagrams in a inline way")
-def get_anagrams(word, inline):
+def get_prefix(word, prefix, inline):
+	anagrams = findAnagramsPrefix(word, prefix)
+	click.echo('Anagrams of {} that starts with {}:'.format(word, prefix))
+	if inline:
+		txt = ', '.join(anagrams)
+		click.echo(txt)
+	else:
+		for anagram in anagrams:
+			click.echo(anagram)
+		click.echo('{} anagrams that starts with {}'.format(len(anagrams), prefix))
+
+@main.command()
+@click.argument('word')
+@click.option('--inline', default=False, help="If True, it will return the anagrams in a inline way")
+def get_all(word, inline):
 	anagrams = findAnagrams(word)
 	click.echo('Anagrams of {}:'.format(word))
 	if inline:
